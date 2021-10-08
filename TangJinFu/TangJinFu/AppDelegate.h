d708c495169c0af9a41c1c3fa59ccd05ed027041
wanglei0218 <40132436+wanglei0218@users.noreply.github.com>
2021-10-08 17:38:19 +0800

    Add files via upload

---
+++ TangJinFu/TangJinFu/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  TangJinFu
+//
+//  Created by mac on 2021/10/8.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
+
+
+
---
+++ TangJinFu/TangJinFu/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  TangJinFu
+//
+//  Created by mac on 2021/10/8.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    
+    
+    
+    return YES;
+}
+
+
+@end
+
+
